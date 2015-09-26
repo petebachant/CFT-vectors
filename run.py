c4d6e3545db14e88df9d71c0bb89b4677156f337
Pete Bachant <petebachant@gmail.com>
2015-09-26 00:31:11 -0400

    Start moving over to matplotlib exclusively thanks to TeX support

--- run.py
+++ run.py
@@ -7,7 +7,52 @@ turbine.
 from __future__ import division, print_function
 import gizeh as gz
 import numpy as np
+import matplotlib
 import matplotlib.pyplot as plt
+import cairocffi as cairo
+
+
+class PDFSurface:
+    """
+    Simple class to allow gizeh to create pdf figures
+    
+    from https://gist.github.com/cosmonaut/31ec5f6370eb891581f1
+    """
+    def __init__(self, name, width, height, bg_color=None):
+        self.width = width
+        self.height = height
+        self._cairo_surface = cairo.PDFSurface(name, width, height)
+
+    def get_new_context(self):
+        """Returns a new context for drawing on the surface."""
+        return cairo.Context(self._cairo_surface)
+
+    def get_npimage(self, transparent=False, y_origin="top"):
+        """Returns a WxHx[3-4] numpy array representing the RGB picture.
+        
+        If `transparent` is True the image is WxHx4 and represents a RGBA 
+        picture,
+        i.e. array[i,j] is the [r,g,b,a] value of the pixel at position [i,j].
+        If `transparent` is false, a RGB array is returned.
+
+        Parameter y_origin ("top" or "bottom") decides whether point (0,0) lies in
+        the top-left or bottom-left corner of the screen.
+        """
+
+        im = 0+np.frombuffer(self._cairo_surface.get_data(), np.uint8)
+        im.shape = (self.height, self.width, 4)
+        im = im[:,:,[2,1,0,3]] # put RGB back in order
+        if y_origin== "bottom":
+            im = im[::-1]
+        return im if transparent else im[:,:, :3]
+
+    def flush(self):
+        """Write the file"""
+        self._cairo_surface.flush()
+
+    def finish(self):
+        """Close the surface"""
+        self._cairo_surface.finish()
 
 
 def mag(v):
@@ -25,7 +70,7 @@ def rotate(v, rad):
     return np.array((x, y))
 
 
-def gen_naca_points(naca="0020", c=100, npoints=100):
+def gen_naca_points(naca="0020", c=100, npoints=100, tuples=True):
     """Generate points for a NACA foil."""
     x = np.linspace(0, 1, npoints)*c
     t = float(naca[2:])/100.0
@@ -33,8 +78,10 @@ def gen_naca_points(naca="0020", c=100, npoints=100):
             + 0.2843*(x/c)**3 - 0.1015*(x/c)**4)
     y = np.append(y, -y[::-1])
     x = np.append(x, x[::-1])
-    points = [(x0, y0) for x0, y0 in zip(x, y)]
-    return points
+    if tuples:
+        return np.array([(x0, y0) for x0, y0 in zip(x, y)])
+    else:
+        return x, y
 
 
 def test_gen_naca_points():
@@ -85,6 +132,12 @@ def make_arrow(xy_start, xy_end, head_angle_deg=25, head_len=40, label="",
                        stroke=color, fill=color)
     return gz.Group((shaft, head))
     
+
+def make_label(text, **kwargs):
+    """Create a label."""
+    label = gz.text(text, fontfamily="times", fontslant="italic", **kwargs)
+    return label
+    
     
 def make_diagram(tsr, theta):
     """Create entire turbine diagram as a Group."""
@@ -96,6 +149,14 @@ def plot_surface(surface):
     plt.figure(figsize=(6, 6))
     plt.axis("off")
     plt.imshow(surface.get_npimage())
+    
+    
+def make_pdf():
+    s = PDFSurface("test.pdf", 600, 600, bg_color=(1, 1, 1))
+    arrow = make_arrow((0, 0), (100, 100), stroke_width=2, head_len=20)
+    arrow.draw(s)
+    s.flush()
+    s.finish()
 
 
 def main():
@@ -113,10 +174,55 @@ def main():
                         color=(0.5, 0.5, 0.5), stroke_width=5)
     arrow.draw(canvas)
     arrow2.draw(canvas)
-    canvas.write_to_png("cft-vectors.png")
     
+    origin_label = make_label(chr(945), fontsize=50, xy=(350, 300))
+    origin_label.draw(canvas)
+
+    canvas.write_to_png("cft-vectors.png")    
     plot_surface(canvas)
 
 
+def plot_radius(ax, theta_deg=0):
+    """Plot radius at given azimuthal angle."""
+    r = 0.495
+    theta_rad = np.deg2rad(theta_deg)
+    x2, y2 = r*np.cos(theta_rad), r*np.sin(theta_rad)
+    ax.plot((0, x2), (0, y2), "k", linewidth=2)
+
+
+def make_naca_path():
+    c = 0.3
+    verts = gen_naca_points(c=c)
+    verts = np.array([rotate(v, -np.pi/2) for v in verts])
+    verts += (0.5, c/4)
+    p = matplotlib.path.Path(verts, closed=True)
+    return p
+        
+    
+def plot_foil_patch():
+    """Plot the foil shape using a matplotlib patch."""
+    p = matplotlib.patches.PathPatch(make_naca_path(), facecolor="gray")
+    fig, ax = plt.subplots(figsize=(6, 6))
+    plot_radius(ax)
+    t_start = ax.transData
+    t = matplotlib.transforms.Affine2D().rotate_deg(0)
+    t_end = t_start + t
+    p.set_transform(t_end)
+    ax.add_patch(p)
+    ax.set_xlim((-1, 1))
+    ax.set_ylim((-1, 1))
+    ax.set_aspect(1)
+    ax.axis("off")
+    
+    # Make relative velocity vector
+#    ax.annotate(r"$\omega r$", xy=(0.5, 0.3/4), xytext=(0.5, 0.25),
+#                arrowprops={"arrowstyle": "->"})
+    ax.arrow(0.5, 0.5, 0, -0.5, head_width=0.06, head_length=0.15, 
+             length_includes_head=True, color="black")
+    ax.text(0.5, 0.5, r"$\omega r$")
+
+
+
 if __name__ == "__main__":
-    main()
+    plt.rcParams["font.size"] = 18
+    plot_foil_patch()
